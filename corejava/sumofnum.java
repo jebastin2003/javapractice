b6f509ca6761180e406b19e28a1a8d841fa863b3
Jebastin <jebastinn2003@gmail.com>
2025-08-22 22:39:44 +0530

    Add files via upload

---
+++ corejava/sumofnum.java
@@ -0,0 +1,18 @@
+package controlstatements;
+
+public class sumofnum {
+
+	public static void main(String[] args) {
+		int i=0;
+		int k=0;
+		while(i<=10) {
+			k=k+i;
+			
+			System.out.println("sum of"+" "+i+" "+"number is"+" "+k);
+			i++;
+		}
+		System.out.println("sum of numbers is"+" "+k);
+
+	}
+
+}
