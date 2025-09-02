28543c1a99f8baac0aaac6884ecabaa62c8fe08b
Jebastin D <jebastinn2003@gmail.com>
2025-09-02 17:03:30 +0530

    Add files via upload

---
+++ OOPSjava/Constructorex1.java
@@ -0,0 +1,18 @@
+package controlstatements;
+
+
+public class Constructorex1 {
+	public Constructorex1() {
+		System.out.println("hello i am constructor");
+		}
+	public void method() {
+		System.out.println("i am method");
+	}
+
+	public static void main(String[] args) {
+		Constructorex1 obj=new Constructorex1();
+		obj.method();
+
+	}
+
+}
