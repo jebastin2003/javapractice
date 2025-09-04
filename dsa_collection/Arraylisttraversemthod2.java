b4064fb441d3db060b4db3f283e57931cb759d97
Jebastin D <jebastinn2003@gmail.com>
2025-09-04 10:52:56 +0530

    Add files via upload

---
+++ dsa_collection/Arraylisttraversemthod2.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Arraylisttraversemthod2 {
+    public static void main(String [] args){
+        ArrayList<String>al=new ArrayList<String>();
+        al.add("gfg");
+        al.add("ide");
+        al.add("courses");
+        for(String i:al){
+            System.out.println(i);
+
+        }
+
+    }
+}
