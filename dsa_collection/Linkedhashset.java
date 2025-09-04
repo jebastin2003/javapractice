dab9d387d992ae7d72e20abc0af2565237c15765
Jebastin D <jebastinn2003@gmail.com>
2025-09-04 10:59:17 +0530

    Add files via upload

---
+++ dsa_collection/Linkedhashset.java
@@ -0,0 +1,12 @@
+import java.util.LinkedHashSet;
+public class Linkedhashset {
+    public static void main(String [] args){
+        LinkedHashSet <Integer>lh=new LinkedHashSet <Integer>();
+        lh.add(10);
+        lh.add(20);
+        lh.add(30);
+        lh.add(10);
+        System.out.println(lh);
+
+    }
+}
