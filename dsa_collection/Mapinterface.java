ea2b09764ed94932cf0cb6352605a72ede540b75
Jebastin D <jebastinn2003@gmail.com>
2025-09-05 00:08:13 +0530

    Add files via upload

---
+++ dsa_collection/Mapinterface.java
@@ -0,0 +1,12 @@
+import java.util.TreeMap;
+import java.util.Map;
+public class Mapinterface {
+    public static void main(String []args){
+        Map<Integer,String>m=new TreeMap<>();
+        m.put(100,"gfg");
+        m.put(200,"course");
+        m.put(160,"hello");
+        System.out.println(m);
+
+    }
+}
