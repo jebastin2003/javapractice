d5f2014ddf6bfae5c071c374c2fefe161a5ca2a3
Jebastin D <jebastinn2003@gmail.com>
2025-09-04 10:54:13 +0530

    Add files via upload

---
+++ dsa_collection/Arraylisttraversemthod5.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Arraylisttraversemthod5 {
+    public static void main(String [] args){
+        ArrayList<Integer>al=new ArrayList<Integer>();
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        al.add(5);
+        al.forEach(x->System.out.println(x));
+        }
+}
