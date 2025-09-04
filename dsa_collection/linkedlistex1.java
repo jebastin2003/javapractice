88ae6c49428c0eb358b3ba0c73a50e8cd1334c5b
Jebastin D <jebastinn2003@gmail.com>
2025-09-04 10:56:10 +0530

    Add files via upload

---
+++ dsa_collection/linkedlistex1.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+public class linkedlistex1 {
+    public static void main(String [] args){
+        LinkedList<Integer>list=new LinkedList<Integer>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        System.out.println(list);
+
+    }
+}
