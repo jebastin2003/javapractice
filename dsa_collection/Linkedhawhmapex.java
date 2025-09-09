5882e57dbac0a05236d1a7e881a7657c52b6f302
Jebastin D <jebastinn2003@gmail.com>
2025-09-09 22:40:46 +0530

    Add files via upload

---
+++ dsa_collection/Linkedhawhmapex.java
@@ -0,0 +1,14 @@
+import java.util.LinkedHashMap;
+public class Linkedhawhmapex {
+    public static void main(String[]args){
+        LinkedHashMap<Integer,String>lhm=new LinkedHashMap<>();
+        lhm.put(1,"jebastin");
+        lhm.put(2,"manoj");
+        lhm.put(3,"tawfiq1");
+        lhm.put(3,"tawfiq");
+        lhm.put(2,"ram");
+        System.out.println(lhm);
+
+    }
+    
+}
