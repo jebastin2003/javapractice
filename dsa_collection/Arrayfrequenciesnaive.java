335f2c222cb771fa947482b026e0b07f2d7db381
Jebastin D <jebastinn2003@gmail.com>
2025-09-11 22:56:48 +0530

    Add files via upload

---
+++ dsa_collection/Arrayfrequenciesnaive.java
@@ -0,0 +1,28 @@
+public class Arrayfrequenciesnaive {
+    static void printfrequencies(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            boolean flag=false;
+            for(int j=0;j<i;j++){
+                if(arr[i]==arr[j]){
+                    flag=true;
+                    break;
+                }
+            }
+            if(flag==true){
+                continue;
+            }
+            int count=1;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+            }
+            System.out.println(arr[i]+" "+count);
+        }
+    }
+    public static void main(String []args){
+        int arr[]={10,5,20,5,10,2};
+        printfrequencies(arr);
+
+    } 
+}
