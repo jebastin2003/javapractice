30289899dc6587c7a52f0575925c8573f49fc4f4
Jebastin D <jebastinn2003@gmail.com>
2025-09-26 17:29:32 +0530

    Add files via upload

---
+++ logicpractice/Starptrn3.java
@@ -0,0 +1,21 @@
+package logicpractice;
+
+public class Starptrn3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int w2=5;
+		for(int i=0;i<=w2;i++) {
+			for(int k=0;k<=w2-i;k++) {
+				System.out.print(" ");
+			}
+			for(int j=0;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+			
+		}
+
+	}
+
+}
